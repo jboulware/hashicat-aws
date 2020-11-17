3bb6aff3782785e4783bd497b36c3fdc3ec38df0
Jason Boulware <jason.boulware@hearst.com>
2020-11-17 13:36:35 -0700

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app. Note that there are seldom good technological solutions to behavior problems.
   <!-- END -->
 
   </div>
